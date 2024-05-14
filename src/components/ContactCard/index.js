4621cdd320a323845879c94ba229a6d05d370110
Suelytohm Oliveira <suelytohm.02@gmail.com>
2024-05-14 01:02:26 -0300

    fix: pixel diferente da borda dos botões

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -8,68 +8,56 @@ import "./style.css";
 
 const ContactCard = ({ contato }) => {
   return (
-    <>
+    <div className="btnContatos">
       {contato && contato === "E-mail" ? (
-        <>
-          <a
-            target="_blank"
-            href="mailto:suelytohm.02@gmail.com?subject=Contato pelo portfólio"
-            rel="noreferrer"
-          >
-            <div className="buttonContato">
-              <CiMail className="icons" size="30px" />
-              <h3>{contato}</h3>
-            </div>
-          </a>
-        </>
+        <a
+          target="_blank"
+          href="mailto:suelytohm.02@gmail.com?subject=Contato pelo portfólio"
+          rel="noreferrer"
+        >
+          <div className="buttonContato">
+            <CiMail className="icons" size="30px" />
+            <h3>{contato}</h3>
+          </div>
+        </a>
       ) : (
         <></>
       )}
       {contato && contato === "WhatsApp" ? (
-        <>
-          <a target="_blank" href="https://wa.me/558791087013" rel="noreferrer">
-            <div className="buttonContato">
-              <FaWhatsapp className="icons" size="30px" />
-              <h3>{contato}</h3>
-            </div>
-          </a>
-        </>
+        <a target="_blank" href="https://wa.me/558791087013" rel="noreferrer">
+          <div className="buttonContato">
+            <FaWhatsapp className="icons" size="30px" />
+            <h3>{contato}</h3>
+          </div>
+        </a>
       ) : (
         <></>
       )}
       {contato && contato === "GitHub" ? (
-        <>
-          <a
-            target="_blank"
-            href="https://github.com/suelytohm"
-            rel="noreferrer"
-          >
-            <div className="buttonContato">
-              <FaGithub className="icons" size="30px" />
-              <h3>{contato}</h3>
-            </div>
-          </a>
-        </>
+        <a target="_blank" href="https://github.com/suelytohm" rel="noreferrer">
+          <div className="buttonContato">
+            <FaGithub className="icons" size="30px" />
+            <h3>{contato}</h3>
+          </div>
+        </a>
       ) : (
         <></>
       )}
       {contato && contato === "LinkedIn" ? (
-        <>
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/suelytohm"
-            rel="noreferrer"
-          >
-            <div className="buttonContato">
-              <FaLinkedin className="icons" size="30px" />
-              <h3>{contato}</h3>
-            </div>
-          </a>
-        </>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/suelytohm"
+          rel="noreferrer"
+        >
+          <div className="buttonContato">
+            <FaLinkedin className="icons" size="30px" />
+            <h3>{contato}</h3>
+          </div>
+        </a>
       ) : (
         <></>
       )}
-    </>
+    </div>
   );
 };
 
